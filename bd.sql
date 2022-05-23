f2c719a35ee052c4be00ef0fe80ba1f0d6099241
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-23 16:24:44 +0300

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Ex3 (
+    id int(10) unsigned PRIMARY KEY AUTO_INCREMENT,
+    fio varchar(25),
+    e-mail varchar(25), 
+    Dat Date,
+    Sex char(1),
+    Biography varchar(100)
+)
