7cc7125923f725e38f2b0cee974e54c5958b370b
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-23 17:01:13 +0300

    Delete bd.sql

--- bd.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Ex3 (
-    id int(10) unsigned PRIMARY KEY AUTO_INCREMENT,
-    fio varchar(25),
-    e-mail varchar(25), 
-    Dat Date,
-    Sex char(1),
-    Biography varchar(100)
-)
